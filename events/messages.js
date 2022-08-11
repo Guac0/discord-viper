00d3e98710b51dcaf56d01f06851e9e4b7f0d142
Guac0 <66752224+Guac0@users.noreply.github.com>
2022-08-11 17:51:43 -0400

    fixed file upload

--- events/messages.js
+++ events/messages.js
@@ -6,10 +6,11 @@ global.viper.on("message", message => {
 
       message.attachments.forEach(function(attachment,id) {
 
-        if( attachment.filename.substr(attachment.filename.lastIndexOf('.') + 1 ) != 'pbo'){
+        if( attachment.name.substr(attachment.name.lastIndexOf('.') + 1 ) != 'pbo'){
           if(global.utils.getEnv("nonPBOwarningUpload")){
             message.reply(":warning: You can only upload PBOs with Viper, upload was ignored");
           }
+          console.log("Denied non-pbo upload");
           return false;
         }
 
@@ -23,27 +24,27 @@ global.viper.on("message", message => {
         global.RP.get(options)
           .then(function (res) {
 
-            message.channel.send(":white_check_mark: Requesting: " + global.utils.strHighlight(attachment.filename) );
+            message.channel.send(":white_check_mark: Requesting: " + global.utils.strHighlight(attachment.name) );
             const buffer = Buffer.from(res, 'utf8');
-            global.FS.writeFileSync(global._tempUploadFolder + attachment.filename, buffer);
+            global.FS.writeFileSync(global._tempUploadFolder + attachment.name, buffer);
             console.log(`Save file in: ${global._tempUploadFolder}`);
-            message.channel.send(":white_check_mark: Downloaded: " + global.utils.strHighlight(attachment.filename) );
+            message.channel.send(":white_check_mark: Downloaded: " + global.utils.strHighlight(attachment.name) );
 
-            console.log(global._tempUploadFolder + attachment.filename,global.utils.getEnv("uploadFolder") + attachment.filename);
+            console.log(global._tempUploadFolder + attachment.name,global.utils.getEnv("uploadFolder") + attachment.name);
 
-            global.FS.copyFile(global._tempUploadFolder + attachment.filename,global._uploadFolder + attachment.filename , (err) => {
+            global.FS.copyFile(global._tempUploadFolder + attachment.name,global._uploadFolder + attachment.name , (err) => {
               if (err) {
                 message.channel.send(err.toString());
               }else{
 
-                message.channel.send(":white_check_mark: " + global.utils.strHighlight(attachment.filename) + " moved to " + global.utils.strHighlight(global._uploadFolder) );
+                message.channel.send(":white_check_mark: " + global.utils.strHighlight(attachment.name) + " moved to " + global.utils.strHighlight(global._uploadFolder) );
 
-                global.FS.unlink(global._tempUploadFolder + attachment.filename, (err) => {
+                global.FS.unlink(global._tempUploadFolder + attachment.name, (err) => {
                   if (err) {
                     console.error(err)
                     message.channel.send( err.toString() );
                   }else{
-                    message.channel.send(":white_check_mark: " + global.utils.strHighlight( attachment.filename ) + " temp file deleted");
+                    message.channel.send(":white_check_mark: " + global.utils.strHighlight( attachment.name ) + " temp file deleted");
                     message.channel.send(":white_check_mark: " + "Done :ok_hand:");
                   }
                 })
