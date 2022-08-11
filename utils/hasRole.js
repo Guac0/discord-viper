9f0dcba932c638553c135fe2b580cf364c085511
Guac0 <66752224+Guac0@users.noreply.github.com>
2022-08-11 17:17:09 -0400

    fix role, again

--- utils/hasRole.js
+++ utils/hasRole.js
@@ -4,11 +4,12 @@
  * @param rolename
  * @returns {boolean}
  */
-function hasRole(msg,id) {
+ function hasRole(msg,rolename) {
 
     //return msg.member.roles.some(role => role.name === name);
-    //return msg.member.roles.cache.find(r => r.name === name)
-    return msg.member.roles.has(id)
+    //return msg.member.roles.cache.find(r => r.name === rolename)
+    //return msg.member.roles.has(id)
+    if (msg.member.roles.cache.some(role => role.name === rolename)) { return true } else { return false }
     //return true;
 
 }
